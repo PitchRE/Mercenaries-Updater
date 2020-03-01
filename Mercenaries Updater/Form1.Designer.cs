0b8897d979a8935fdd4151dc9bdbd3619d85bd11
PitchRE <hi@pitchblack.eu>
2020-03-01 13:49:07 +0100

    Dodaj pliki projektów.

---
+++ Mercenaries Updater/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Mercenaries_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.version_label = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 256);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(455, 38);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(455, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Remote Version: ";
+            // 
+            // version_label
+            // 
+            this.version_label.AutoSize = true;
+            this.version_label.Location = new System.Drawing.Point(106, 21);
+            this.version_label.Name = "version_label";
+            this.version_label.Size = new System.Drawing.Size(53, 13);
+            this.version_label.TabIndex = 3;
+            this.version_label.Text = "Unknown";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(452, 80);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 306);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.version_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label version_label;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Mercenaries Updater/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Mercenaries_Updater
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        List<string> links = new List<string>(new string[] {});
+        int number = 0;
+        string checksum;
+        int id = 0;
+        string escapeString = @"../";
+
+     
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+
+            try
+            {
+                string version = Get(@"http://localhost/data.txt");
+                version = version.Substring(version.LastIndexOf(':') + 1);
+                version_label.Text = version;
+                checksum = Get(@"https://raw.githubusercontent.com/PitchRE/Mercenaries-Client/master/checksum.txt");
+                button1.Enabled = true;
+            } catch
+            {
+                version_label.Text = "Error. Couldn't connect to server.";
+            }
+    
+        }
+   
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            string dir = System.Reflection.Assembly.GetEntryAssembly().Location;
+
+            button1.Enabled = false;
+            Task task1 = Task.Factory.StartNew(() => CheckChecksum());
+            Task task2 = task1.ContinueWith(antTask => DownloadPrepare());
+       
+
+
+
+
+
+
+        }
+
+
+        private  void DownloadPrepare()
+        {
+       
+            this.Invoke((MethodInvoker)delegate {
+                progressBar1.Maximum = links.Count;
+            });
+
+            foreach (string link in links)
+            {
+          
+                Debug.WriteLine(link);
+                DownloadFile(link, id);
+            }
+        }
+
+
+        public string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+
+            string mynumber = ((System.Net.WebClient)(sender)).QueryString["number"];
+            number++;
+            this.Invoke((MethodInvoker)delegate {
+                progressBar1.Value = number;
+                if (progressBar1.Value == progressBar1.Maximum) MessageBox.Show("Update Finished.");
+            });
+            Debug.WriteLine($"Finished {number}/{links.Count}");
+        }
+
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+
+
+        }
+        private Task DownloadFile(string link, int value)
+        {
+           
+            WebClient client = new WebClient();
+            string filename = link;
+            link = @"https://raw.githubusercontent.com/PitchRE/Mercenaries-Client/master/" + link;
+            Debug.WriteLine(filename);
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadFileCompleted += Client_DownloadFileCompleted;
+            client.DownloadFileAsync(new Uri(link.Replace("../", "")), filename);
+            Debug.WriteLine($"Downloading {link}");
+            this.Invoke((MethodInvoker)delegate {
+                richTextBox1.AppendText($"Downloaded {filename} \n");
+            });
+
+            return Task.FromResult<object>(null);
+        }
+
+        public Boolean CheckChecksum()
+        {
+       
+            using (StringReader reader = new StringReader(checksum))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] tmp = line.Split(new string[] { ">>>" }, StringSplitOptions.None);
+                    string path = tmp[0];
+                    string hash = tmp[1].Trim();
+                    ValidHash(path, hash);
+
+                }
+            }
+            return true;
+        }
+
+        public Boolean ValidHash(string path, string hash)
+        {
+            
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                string filepath = escapeString + path;
+              
+
+                if (File.Exists(filepath))
+                {
+                        var fi1 = new FileInfo(filepath);
+                        FileStream fileStream = fi1.Open(FileMode.Open);
+                        byte[] hashValue = mySHA256.ComputeHash(fileStream);
+                        fileStream.Close();
+                        string StringByte = ReadableHash(hashValue);
+                
+                    if (StringByte == hash)
+                    {
+                        Console.WriteLine($"True: {filepath}");
+                  
+
+
+
+                        return true;
+                    } else
+                    {
+                        links.Add(filepath);
+                        Console.WriteLine($"False: {filepath}");
+                        this.Invoke((MethodInvoker)delegate {
+                            richTextBox1.AppendText($"Corrupted: {filepath} \n");
+                        });
+                        return false;
+                    }
+                } else
+                {
+                    links.Add(filepath);
+                    Console.WriteLine($"False No File: {filepath}");
+                    this.Invoke((MethodInvoker)delegate {
+                        richTextBox1.AppendText($"No File: {filepath} \n");
+                    });
+                    return false;
+                }
+                 
+
+
+            }
+        }
+        static string ReadableHash(byte[] array)
+        {
+            StringBuilder myStringBuilder = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+            {
+                myStringBuilder.Append($"{array[i]:X2}");
+                /*     if ((i % 4) == 3) myStringBuilder.Append(" ");*/
+            }
+            return myStringBuilder.ToString();
+
+        }
+
+    }
+
+}
